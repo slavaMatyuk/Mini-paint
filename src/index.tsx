c5329aa47cfefac371eae88a2d9e477eb41c191f
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2021-10-05 15:26:45 +0300

    feat: set firebase up

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import * as firebase from 'firebase/app';
 import App from './App';
 import GlobalStyles from './GlobalStyles';
 import theme from './Theme';
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
