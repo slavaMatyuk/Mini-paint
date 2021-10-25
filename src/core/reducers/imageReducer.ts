f223100debcb654ca694c30256e2d5cd99a7c802
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2021-10-25 18:58:11 +0300

    feat: expand image-reducer

--- src/core/reducers/imageReducer.ts
+++ src/core/reducers/imageReducer.ts
@@ -4,6 +4,14 @@ import {
   SORT_IMAGES,
   GET_USER_IMAGES_FROM_DB,
   DEL_USER_IMAGE_FROM_DB,
+  SAVE_IMAGE_SUCCEEDED,
+  SAVE_IMAGE_FAILED,
+  GET_ALL_IMAGES_FROM_DB_SUCCEEDED,
+  GET_ALL_IMAGES_FROM_DB_FAILED,
+  DEL_USER_IMAGE_FROM_DB_SUCCEEDED,
+  DEL_USER_IMAGE_FROM_DB_FAILED,
+  GET_USER_IMAGES_FROM_DB_SUCCEEDED,
+  GET_USER_IMAGES_FROM_DB_FAILED,
 } from '../actions/imageContainerActions';
 
 interface ImageAction {
@@ -18,21 +26,23 @@ interface ImageAction {
 }
 
 export interface ImageState {
+  loading: boolean,
   error: boolean,
   deleteWithID: number | null,
-  deleteWithURL: string | null,
   subContexDataUrl: string,
   imageName: string,
+  imagesProfData: [],
   imagesData: [],
   sortedImagesData: [],
 }
 
 const initialState: ImageState = {
+  loading: false,
   error: false,
   deleteWithID: null,
-  deleteWithURL: null,
   subContexDataUrl: '',
   imageName: '',
+  imagesProfData: [],
   imagesData: [],
   sortedImagesData: [],
 };
@@ -45,18 +55,46 @@ export const imageReducer = (state = initialState, action: ImageAction): object 
         subContexDataUrl: action.dataUrl,
         error: false,
       };
+
+    case SAVE_IMAGE_SUCCEEDED:
+      return {
+        ...state, loading: false, error: false,
+      };
+    case SAVE_IMAGE_FAILED:
+      return {
+        ...state, loading: false, error: true,
+      };
     case GET_ALL_IMAGES_FROM_DB:
-      return { ...state, imagesData: action.payload || [] };
+      return {
+        ...state, loading: true, error: false,
+      };
+    case GET_ALL_IMAGES_FROM_DB_SUCCEEDED:
+      return { ...state, imagesData: action.payload };
+    case GET_ALL_IMAGES_FROM_DB_FAILED:
+      return {
+        ...state, loading: false, error: true,
+      };
     case SORT_IMAGES:
-      return { ...state, sortedImagesData: action.data || [] };
-    case GET_USER_IMAGES_FROM_DB:
-      return { ...state, imagesData: action.payload || [] };
+      return { ...state, sortedImagesData: action.data };
     case DEL_USER_IMAGE_FROM_DB:
+      return { ...state, loading: true };
+    case DEL_USER_IMAGE_FROM_DB_SUCCEEDED:
       return {
         ...state,
-        imagesData: state.imagesData.filter((img: {id: string | number}) => img.id !== state.deleteWithID),
+        imagesProfData: state.imagesProfData.filter((img: {id: string | number}) => img.id !== state.deleteWithID),
+        loading: false,
         deleteWithID: null,
       };
+    case DEL_USER_IMAGE_FROM_DB_FAILED:
+      return { ...state, loading: false };
+    case GET_USER_IMAGES_FROM_DB:
+      return {
+        ...state, loading: true, error: false,
+      };
+    case GET_USER_IMAGES_FROM_DB_SUCCEEDED:
+      return { ...state, loading: false, imagesProfData: action.payload };
+    case GET_USER_IMAGES_FROM_DB_FAILED:
+      return { ...state, loading: false };
     default:
       return state;
   }
