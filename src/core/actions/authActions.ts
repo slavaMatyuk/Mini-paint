824153b51147d5c0002f0227f6b533befa3d3a55
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2021-10-25 17:50:08 +0300

    feat: expand action-creators

--- src/core/actions/authActions.ts
+++ src/core/actions/authActions.ts
@@ -1,7 +1,14 @@
 export const CREATE_USER_WITH_REGISTER = 'AUTH/CREATE_USER_WITH_REGISTER';
+export const CREATE_USER_WITH_REGISTER_SUCCEEDED = 'AUTH/CREATE_USER_WITH_REGISTER_SUCCEEDED';
+export const CREATE_USER_WITH_REGISTER_FAILED = 'AUTH/CREATE_USER_WITH_REGISTER_FAILED';
 export const LOG_IN = 'AUTH/LOG_IN';
+export const LOG_IN_SUCCEEDED = 'AUTH/LOG_IN_SUCCEEDED';
+export const LOG_IN_FAILED = 'AUTH/LOG_IN_FAILED';
 export const LOG_OUT = 'AUTH/LOG_OUT';
+export const LOG_OUT_SUCCEEDED = 'AUTH/LOG_OUT_SUCCEEDED';
+export const LOG_OUT_FAILED = 'AUTH/LOG_OUT_FAILED';
 export const SET_AUTH = 'AUTH/SET_AUTH';
+export const VERIFY_AUTH = 'AUTH/VERIFY_AUTH';
 export const SET_ERROR_MESSAGE = 'AUTH/SET_ERROR_MESSAGE';
 
 export const createUserAction = (payload: {email: string, password: string}) => (
@@ -19,3 +26,27 @@ export const setErrorAction = (error: {code: string, message: string} | unknown)
 export const setAuthAction = (userName: string, userID: string) => ({ type: SET_AUTH, userName, userID });
 
 export const logOutAction = () => ({ type: LOG_OUT });
+
+export const createUserSucceededAction = (payload: { userName: string, userID: string}) => (
+  { type: CREATE_USER_WITH_REGISTER_SUCCEEDED, payload }
+);
+
+export const createUserFailedAction = (error: {code: string, message: string} | unknown) => (
+  { type: CREATE_USER_WITH_REGISTER_FAILED, error }
+);
+
+export const logInSucceededAction = (payload: { userName: string, userID: string}) => (
+  { type: LOG_IN_SUCCEEDED, payload }
+);
+
+export const logInFailedAction = (error: {code: string, message: string} | unknown) => (
+  { type: LOG_IN_FAILED, error }
+);
+
+export const logOutSucceededAction = () => ({ type: LOG_OUT_SUCCEEDED });
+
+export const logOutFailedAction = (error: {code: string, message: string} | unknown) => (
+  { type: LOG_OUT_FAILED, error }
+);
+
+export const verifyAuthAction = () => ({ type: VERIFY_AUTH });
--- src/core/actions/imageContainerActions.ts
+++ src/core/actions/imageContainerActions.ts
@@ -1,10 +1,18 @@
 export const GET_USER_NAME = 'IMAGE/GET_USER_NAME';
 export const GET_USER_ID = 'IMAGE/GET_USER_ID';
 export const GET_USER_IMAGES_FROM_DB = 'IMAGE/GET_USER_IMAGES_FROM_DB';
-export const DEL_USER_IMAGE_FROM_DB = 'IMAGE/DEL_USER_IMAGE_FROM_DB';
+export const GET_USER_IMAGES_FROM_DB_SUCCEEDED = 'IMAGE/GET_USER_IMAGES_FROM_DB_SUCCEEDED';
+export const GET_USER_IMAGES_FROM_DB_FAILED = 'IMAGE/GET_USER_IMAGES_FROM_DB_FAILED';
 export const GET_ALL_IMAGES_FROM_DB = 'IMAGE/GET_ALL_IMAGES_FROM_DB';
+export const GET_ALL_IMAGES_FROM_DB_SUCCEEDED = 'IMAGE/GET_ALL_IMAGES_FROM_DB_SUCCEEDED';
+export const GET_ALL_IMAGES_FROM_DB_FAILED = 'IMAGE/GET_ALL_IMAGES_FROM_DB_FAILED';
 export const SORT_IMAGES = 'IMAGE/SORT_IMAGES';
 export const SET_DATA_URL = 'IMAGE/SET_DATA_URL';
+export const SAVE_IMAGE_SUCCEEDED = 'IMAGE/SAVE_IMAGE_SUCCEEDED';
+export const SAVE_IMAGE_FAILED = 'IMAGE/SAVE_IMAGE_FAILED';
+export const DEL_USER_IMAGE_FROM_DB = 'IMAGE/DEL_USER_IMAGE_FROM_DB';
+export const DEL_USER_IMAGE_FROM_DB_SUCCEEDED = 'IMAGE/DEL_USER_IMAGE_FROM_DB_SUCCEEDED';
+export const DEL_USER_IMAGE_FROM_DB_FAILED = 'IMAGE/DEL_USER_IMAGE_FROM_DB_FAILED';
 
 export const getUserNameAction = () => ({ type: GET_USER_NAME });
 
@@ -27,3 +35,31 @@ export const sortImagesAction = (data: object) => ({ type: SORT_IMAGES, data });
 export const setDataUrlAction = (dataUrl: string, userID: string, userName: string) => ({
   type: SET_DATA_URL, dataUrl, userID, userName,
 });
+
+export const saveImageSucceededAction = () => ({ type: SAVE_IMAGE_SUCCEEDED });
+
+export const saveImageFailedAction = (payload: {code: string, message: string} | unknown) => (
+  { type: SAVE_IMAGE_FAILED, payload }
+);
+
+export const delUserImageFromDbSucceededAction = () => ({ type: DEL_USER_IMAGE_FROM_DB_SUCCEEDED });
+
+export const delUserImageFromDbFailedAction = (payload: {code: string, message: string} | unknown) => (
+  { type: DEL_USER_IMAGE_FROM_DB_FAILED, payload }
+);
+
+export const getUserImagesFromDbSucceededAction = (payload: [object] | unknown) => (
+  { type: GET_USER_IMAGES_FROM_DB_SUCCEEDED, payload }
+);
+
+export const getUserImagesFromDbFailedAction = (payload: {code: string, message: string} | unknown) => (
+  { type: GET_USER_IMAGES_FROM_DB_FAILED, payload }
+);
+
+export const getAllImagesFromDbSucceededAction = (payload: object | unknown) => (
+  { type: GET_ALL_IMAGES_FROM_DB_SUCCEEDED, payload }
+);
+
+export const getAllImagesFromDbFailedAction = (payload: {code:string, message: string} | unknown) => (
+  { type: GET_ALL_IMAGES_FROM_DB_FAILED, payload }
+);
