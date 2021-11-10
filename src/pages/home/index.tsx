92afa7318fbd500d56c1f8e7529f1b8bccb04854
slavaMatyuk <vyacheslav.matyuk@innowise-group.com>
2021-11-10 15:30:33 +0300

    fix: sorting and redirections on HomePage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, {
+  useCallback, useEffect, useRef, useState, KeyboardEvent,
+} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,34 +9,41 @@ import {
 } from '../../core/actions/imageContainerActions';
 import StyledGallery from '../../core/components/Canvas/styles/StyledGallery';
 import StyledGalleryWrapper from '../../core/components/Canvas/styles/StyledGalleryWrapper';
-import Input from '../../core/components/Input';
-import Spinner from '../../core/components/Spinner';
 import StyledButton from '../../core/components/styles/buttons/StyledButton';
 import StyledFlexRow from '../../core/components/styles/common/StyledFlexRow';
-import StyledForm from '../../core/components/styles/forms/StyledForm';
 import RoutesConst from '../../core/constants/routesConst';
 import sortImages from '../../core/helpers/sortImages';
 import { AppState } from '../../core/interfaces';
 import StyledGalleryTitle from './styles/StyledGalleryTitle';
 import StyledHomeImagesWrap from './styles/StyledHomeImagesWrap';
 import StyledHomeWrapper from './styles/StyledHomeWrapper';
+import StyledInputWrapper from './styles/StyledInputWrapper';
 import TransparentWrapper from './styles/TransparentWrapper';
 
 const HomePage: React.FC = () => {
   const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(false);
+  const inputRef = useRef(null);
   const [inputValue, setInputValue] = useState('');
   const imagesData = useSelector((state: AppState) => state.images.imagesProfData);
 
-  const sortImagesData = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-    return dispatch(sortImagesAction(sortImages(imagesData, e.target.value)));
-  };
+  const handleInputChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.currentTarget.value);
+  }, []);
+
+  const handleEnterClick = useCallback(async () => {
+    if (!inputValue) {
+      await dispatch(getAllImagesFromDbAction());
+    } else {
+      await dispatch(sortImagesAction(sortImages(imagesData, inputValue)));
+    }
+  }, [inputValue, imagesData, dispatch]);
+
+  const onKeyDownHandler = useCallback(async (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') await handleEnterClick();
+  }, [handleEnterClick]);
 
   useEffect(() => {
-    setIsLoading(true);
     dispatch(getAllImagesFromDbAction());
-    setIsLoading(false);
   }, [dispatch]);
 
   return (
@@ -49,43 +58,46 @@ const HomePage: React.FC = () => {
           Editor
         </StyledButton>
       </NavLink>
-      <StyledForm>
-        <Input
-          type="text"
-          onChange={sortImagesData}
-          name="email"
-          placeholder=""
-          value={inputValue}
-          className=""
-          label="Enter user"
-        />
-      </StyledForm>
+      <StyledInputWrapper>
+        <label htmlFor="search">
+          Enter user
+          <input
+            ref={inputRef}
+            type="text"
+            id="search"
+            name="search"
+            placeholder="and push Enter to sort"
+            onChange={handleInputChange}
+            onKeyDown={onKeyDownHandler}
+            value={inputValue}
+          />
+        </label>
+      </StyledInputWrapper>
       <StyledGalleryWrapper>
-        {isLoading && <Spinner />}
         <TransparentWrapper>
           {
-          imagesData.map((elem: { userName: string, images: [] }, key) => {
-            if (elem.images && elem.images.length) {
-              return (
-                <div key={+key}>
-                  <StyledFlexRow>
-                    <StyledGalleryTitle>{elem.userName}</StyledGalleryTitle>
-                  </StyledFlexRow>
-                  {
-                  elem.images.map((image: { id: string, imgUrl: string, userName: string }, key2: number) => (
-                    <StyledHomeImagesWrap key={+key2}>
-                      <StyledGallery>
-                        <img src={image.imgUrl} alt={image.imgUrl} />
-                      </StyledGallery>
-                    </StyledHomeImagesWrap>
-                  ))
-                }
-                </div>
-              );
-            }
-            return null;
-          })
-        }
+            imagesData.map((elem: { userName: string, images: [] }, key) => {
+              if (elem.images && elem.images.length) {
+                return (
+                  <div key={+key}>
+                    <StyledFlexRow>
+                      <StyledGalleryTitle>{elem.userName}</StyledGalleryTitle>
+                    </StyledFlexRow>
+                    {
+                      elem.images.map((image: { id: string, imgUrl: string, userName: string }, key2: number) => (
+                        <StyledHomeImagesWrap key={+key2}>
+                          <StyledGallery>
+                            <img src={image.imgUrl} alt={image.imgUrl} />
+                          </StyledGallery>
+                        </StyledHomeImagesWrap>
+                      ))
+                    }
+                  </div>
+                );
+              }
+              return null;
+            })
+          }
         </TransparentWrapper>
       </StyledGalleryWrapper>
     </StyledHomeWrapper>
